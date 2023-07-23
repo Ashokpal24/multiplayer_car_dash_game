2f20ec7823553c4051288dce811cc826ee693188
Ashokpal24 <88579810+Ashokpal24@users.noreply.github.com>
2023-07-23 08:13:29 +0000

    added network code

--- public/client.js
+++ public/client.js
@@ -1,5 +1,15 @@
 import * as THREE from "three";
+
+let keys = {
+  KeyW: 0,
+  KeyS: 0,
+  KeyD: 0,
+  KeyA: 0,
+};
+
 const socket = io();
+const direction = new THREE.Vector3();
+let playersCurrState = {};
 
 class CreateScene {
   constructor() {
@@ -13,7 +23,7 @@ class CreateScene {
     this.renderer.shadowMap.enabled = true;
     this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
     this.renderer.setPixelRatio(window.devicePixelRatio);
-    this.renderer.setSize(window.innerWidth, window.innerWidth);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(this.renderer.domElement);
 
     this.light.position.set(20, 100, 20);
@@ -22,12 +32,10 @@ class CreateScene {
     this.light.shadow.bias = -0.001;
     this.light.shadow.mapSize.width = 4096;
     this.light.shadow.mapSize.height = 4096;
-    this.light.shadow.camera.near = 0.1;
-    this.light.shadow.camera.far = 500.0;
     this.light.shadow.camera.near = 0.5;
-    this.light.shadow.camera.far = 500.0;
-    this.light.shadow.camera.left = 50;
-    this.light.shadow.camera.right = -50;
+    this.light.shadow.camera.far = 100.0;
+    this.light.shadow.camera.left = 100;
+    this.light.shadow.camera.right = -100;
     this.light.shadow.camera.top = 50;
     this.light.shadow.camera.bottom = -50;
     this.newScene.add(this.light);
@@ -59,7 +67,7 @@ class TPcamera {
   }
 }
 class AddPlayer {
-  constructor(spawnPoint) {
+  constructor(spawnPoint, newColor) {
     this.spawnPoint = spawnPoint;
     this.moveSpeed = 0.5;
     this.rotationSpeed = 0.05;
@@ -68,7 +76,9 @@ class AddPlayer {
     this.translation = new THREE.Vector3();
     this.targetQuaternion = new THREE.Quaternion();
     this.geometry = new THREE.BoxGeometry(2, 1, 3);
-    this.material = new THREE.MeshStandardMaterial({ color: 0x56d21ff });
+    this.material = new THREE.MeshStandardMaterial({
+      color: Number(newColor),
+    });
     this.player = new THREE.Mesh(this.geometry, this.material);
     this.init();
   }
@@ -97,21 +107,15 @@ class AddPlayer {
   }
 }
 
-let keys = {
-  KeyW: 0,
-  KeyS: 0,
-  KeyD: 0,
-  KeyA: 0,
-};
-const direction = new THREE.Vector3();
 const newEnv = new CreateScene();
 const TPcam = new TPcamera();
-const newPlayer = new AddPlayer(new THREE.Vector3(0, 0.5, 0));
+const newPlayer = new AddPlayer(new THREE.Vector3(0, 0.5, 0), "0x0373fc");
 
 const plane = new THREE.Mesh(
   new THREE.PlaneGeometry(100, 100, 10, 10),
   new THREE.MeshStandardMaterial({ color: 0xf3491ff })
 );
+
 plane.castShadow = false;
 plane.receiveShadow = true;
 plane.rotation.x = -Math.PI / 2;
@@ -164,11 +168,51 @@ function handleAllInput() {
 }
 handleAllInput();
 
+//Network codes-----------------------------------//
 socket.on("connect", () => {
-  console.log(socket.id);
+  console.log("my id: ", socket.id);
   newEnv.addToScene(TPcam.outerGroup);
   newEnv.addToScene(newPlayer.player);
+  playersCurrState[socket.id] = newPlayer;
 });
+
+socket.on("newPlayerJoined", (data) => {
+  for (const clientID in data) {
+    const { currDir, currPos } = data[clientID];
+    if (clientID != Object.keys(playersCurrState)) {
+      let newCLientInst = new AddPlayer(
+        new THREE.Vector3(currPos.x, currPos.y, currPos.z),
+        "0xfc035e"
+      );
+      newCLientInst;
+      newEnv.addToScene(newCLientInst.player);
+      newCLientInst.inputHandling(
+        new THREE.Vector3(currDir.z, currDir.y, currDir.z)
+      );
+      playersCurrState[clientID] = newCLientInst;
+    }
+  }
+  console.log(playersCurrState);
+});
+
+socket.on("playerMoved", (data) => {
+  if (data.id !== socket.id) {
+    console.log(data.id, socket.id);
+
+    playersCurrState[data.id].inputHandling(
+      new THREE.Vector3(data.currDir.z, data.currDir.y, data.currDir.z)
+    );
+  }
+});
+socket.on("playerDisconnected", (data) => {
+  console.log(data);
+});
+setInterval(() => {
+  socket.emit("updatePlayerState", {
+    direction: direction,
+    position: newPlayer.player.position,
+  });
+}, 50);
 
 function update() {
   let Zstrength = keys["KeyW"] - keys["KeyS"];
--- server.js
+++ server.js
@@ -6,10 +6,29 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 const port = 3000;
-
+let players = {};
 app.use(express.static(__dirname + "/public"));
 io.on("connection", (socket) => {
   console.log("New player connected: " + socket.id);
+  players[socket.id] = {
+    currDir: { x: 0, y: 0, z: 0 },
+    currPos: { x: 0, y: 0, z: 0 },
+  };
+  io.emit("newPlayerJoined", players);
+
+  socket.on("updatePlayerState", (data) => {
+    if (players[socket.id]) {
+      players[socket.id].currDir = data.direction;
+      players[socket.id].currPos = data.position;
+      io.emit("playerMoved", { id: socket.id, ...players[socket.id] });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`Player ${socket.id} disconnected`);
+    delete players[socket.id];
+    io.emit("playerDisconnected", socket.id);
+  });
 });
 server.listen(port, () => {
   console.log("Server running on port " + port);
